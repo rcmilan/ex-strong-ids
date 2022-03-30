0806204b38d184bb5e6ffec33fb1fdb0ba57096d
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-30 08:36:25 -0300

    camada de acesso ao banco exe

---
+++ SID.Infra/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+using SID.Infra;
+
+string connString = "server=localhost;port=3306;userid=mysqlusr;password=password;database=typedids;";
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => services.AddDatabaseModule(connString))
+    .Build();
+
+await host.RunAsync();
